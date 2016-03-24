65c22fd756b0a0c9bb4a08d04e75f35e55e95385
nadar <git@nadar.io>
2016-03-24 12:55:53 +0100

    added css for auth form

--- src/views/default/_login.php
+++ src/views/default/_login.php
@@ -1,18 +1,20 @@
 <html>
 	<head>
-		<title>Basic Auth</title>
+		<title><?= Yii::$app->siteTitle; ?> Auth</title>
 	</head>
 	<body>
-		<h1>LUYA BASIC AUTH</h1>
-<p>Please enter your login password here:</p>
-
-    <form method="post">
-    	<input type="password" name="authPassword" /><input type="submit" value="submit" />
-    </form>	
+		<div style="margin:20px; padding:20px; width:500px; background-color:#F0F0F0; border:1px solid #CCC;">
+    		<h1 style="font-size:20px;"><?= Yii::$app->siteTitle; ?></h1>
+    		<p>Please enter a valid password to access this website:</p>
     
-    <?php if ($error): ?>
-    	<p style="color:red;">The entered password is wrong.</p>
-    <?php endif; ?>
+            <form method="post">
+            	<input type="password" name="authPassword" style="padding:4px; width:200px;" /> <input type="submit" value="Login" style="padding:4px;" />
+            </form>	
+        
+            <?php if ($error): ?>
+            	<p style="color:red;">&raquo; The provided password is not correct.</p>
+            <?php endif; ?>
+    	</div>
     
 	</body>
 </html>
