620566e6d445075bfc53d51c67591b6f3f46d0dd
Basil Suter <git@nadar.io>
2016-08-31 13:12:27 +0200

    psr2 fix

--- src/Module.php
+++ src/Module.php
@@ -22,18 +22,18 @@ class Module extends \luya\base\Module implements BootstrapInterface
     
     public function bootstrap($app)
     {
-        $app->on(Application::EVENT_BEFORE_ACTION, function($event) {
+        $app->on(Application::EVENT_BEFORE_ACTION, function ($event) {
             if (!$event->sender->request->isConsoleRequest) {
                 if (
-                    $event->sender->controller->module instanceof \luya\base\Module && 
+                    $event->sender->controller->module instanceof \luya\base\Module &&
                     $event->sender->controller->module->isAdmin === false &&
                     $event->sender->controller->module->id !== $this->id) {
-                        if (!$event->sender->session->get('basicAuthSuccess', false)) {
-                            $event->isValid = false;
-                            return $event->sender->response->redirect(['/basicauth/default/index']);
-                        } else {
-                            Yii::info('User has been authentifacted trough luya module basic app', __METHOD__);
-                        }
+                    if (!$event->sender->session->get('basicAuthSuccess', false)) {
+                        $event->isValid = false;
+                        return $event->sender->response->redirect(['/basicauth/default/index']);
+                    } else {
+                        Yii::info('User has been authentifacted trough luya module basic app', __METHOD__);
+                    }
                 }
             }
         });
