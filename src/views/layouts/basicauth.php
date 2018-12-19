45550764b8fb7a0205f40b330a76e759cb73d1f2
Basil Suter <git@nadar.io>
2018-12-19 20:53:11 +0100

    add scale options

--- src/views/layouts/basicauth.php
+++ src/views/layouts/basicauth.php
@@ -9,9 +9,11 @@ BasicAuthAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?= Yii::$app->composition->langShortCode; ?>">
 <head>
     <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
