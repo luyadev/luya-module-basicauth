4e2dc74f9ebde4fd6f21540f132e443441481586
Basil Suter <git@nadar.io>
2018-12-19 16:41:29 +0100

    add de translations

---
+++ src/messages/de/basicauth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'index_title' => 'Autorisierung',
+  'index_text' => 'Bitte geben Sie das Passwort für den Zugriff auf diese Website ein.',
+  'index_form_submit_label' => 'Weiter',
+  'model_input_label' => 'Passwort',
+  'model_input_hint' => 'Das Passwort wurde vom Website-Betreiber erstellt und kann bei diesem angefordert werden.',
+  'model_input_wrong_password' => 'Das Passwort ist ungültig.',
+  'ssl_info' => 'Die Website verwendet eine verschlüsselte sichere Verbindung.'
+];
--- src/messages/en/basicauth.php
+++ src/messages/en/basicauth.php
@@ -2,10 +2,10 @@
 
 return [
     'index_title' => 'Authorization',
-    'index_text' => 'Please enter a valid password to access this website.',
+    'index_text' => 'Please enter a password to access this website.',
     'index_form_submit_label' => 'Proceed',
     'model_input_label' => 'Password',
     'model_input_hint' => 'The password has been created by the site owner and can be requested there.',
-    'model_input_wrong_password' => 'The given password is not correct.',
+    'model_input_wrong_password' => 'The password is not correct.',
     'ssl_info' => 'Website uses encrypted secure connection.'
 ];
